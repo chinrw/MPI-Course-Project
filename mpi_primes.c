6b562ea63425a0bc2ebd057642ef62d88f470121
enjoybeta <goujiashun@gmail.com>
2018-04-22 15:22:43 -0400

    v0.2
    total rework

--- mpi_primes.c
+++ mpi_primes.c
@@ -1,56 +1,74 @@
-#include <stdio.h>
-#include <mpi.h>
+# include <mpi.h>
+# include <stdio.h>
 
-const unsigned int N = 1000;
+#define MAX_NUMBER 1000000
+#define DISPLAY_PROGRESS 1
+#define DISPLAY_FACTORY 10
 
-int find_prime(unsigned int x){
-    for(unsigned int i = 2; i < x; ++i){
-        if(x%i == 0){
+unsigned int prime_number(unsigned int n, int id, int numProcesses);
+int isprime(unsigned int n);
+
+int main() {
+    int id;
+    int numProcesses;
+    int display_factor;
+    unsigned int n;
+    unsigned int primes;
+    unsigned int primes_partial;
+
+    if(DISPLAY_PROGRESS){
+        n = 1;
+    }
+    else{
+        n = MAX_NUMBER;
+    }
+    
+    display_factor = DISPLAY_FACTORY;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    if(id == 0) {
+        printf ( "%d processes available\n\n", numProcesses );
+    }
+
+    while(n <= MAX_NUMBER){
+        MPI_Bcast (&n, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+
+        primes_partial = prime_number (n, id, numProcesses);
+        MPI_Reduce (&primes_partial, &primes, 1, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        if (id == 0){
+            printf ("%10d    %10d\n", n, primes);
+        }
+        n = n * display_factor;
+    }
+
+    if(id == 0) {
+        printf ("\nTotal number of prime:%u\n",primes);
+    }
+    MPI_Finalize ();
+    
+    return 0;
+}
+
+unsigned int prime_number(unsigned int n, int id, int numProcesses) {
+    unsigned int total = 0;
+
+    for(unsigned int i = 2 + id; i <= n; i += numProcesses) {
+        if(isprime(i)){
+            total += 1;
+        }
+    }
+    return total;
+}
+
+int isprime(unsigned int n) {
+    for(unsigned int i = 2; i < n; ++i) {
+        if(( n % i ) == 0) {
             return 0;
         }
     }
     return 1;
-}
-
-int main(int argc, char *argv[]) {
-    int myid, numprocs, source;
-
-    MPI_Status status;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-
-    printf("I'm %d of %d\n", myid, numprocs);
-    unsigned int start = myid * (N / numprocs), stop = (myid + 1) * (N / numprocs);
-    if (myid == numprocs - 1){
-        stop = N;
-    }
-    printf("start from %d to %d\n", start, stop);
-    unsigned int ans = 0, i;
-    for (i = start; i < stop; i++){
-        if (find_prime(i)){
-            ans += 1;
-        }
-    }
-    //printf("%d finished calculation with %d numbers\n", myid, ans);
-
-    if (myid != 0)
-    {
-        MPI_Send(&ans, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
-    else
-    {
-        unsigned int tmp;
-        for (source = 1; source < numprocs; source++)
-        {
-            MPI_Recv(&tmp, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-            printf("from %d: %d\n", source, tmp);
-            ans += tmp;
-        }
-        printf("final ans: %d\n", ans);
-    }
-
-    MPI_Finalize();
-    return 0;
 }
